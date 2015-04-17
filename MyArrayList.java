589fd6c222e8328719f917b9482d0d1a79ed7354
Brian McEntee <brian.m.mcentee@gmail.com>
2015-04-17 18:34:44 -0400

    skeleton completed

---
+++ MyArrayList.java
@@ -0,0 +1,105 @@
+/**
+	This will simulate Java's ArrayList class via an array based
+	implementation
+	@author Brian McEntee
+	@version 1.0
+*/
+
+public class MyArrayList {
+
+	/**	Creates a new object with initial capacity of 16*/
+	public MyArrayList() {
+		//TODO
+	}
+
+	/** Appends a new object to the end of the list
+		@param o the object to be added to the list */
+	public void add(Object o) {
+		//TODO
+	}
+
+	/** Appends a new object to the list at the given index
+		@param index the integer index in the list the object is placed 
+		@param o the object to be placed in the list */
+	public void add(int index, Object o) {
+		//TODO
+	}
+
+	/** Clears all the objects from the list */
+	public void clear() {
+		//TODO
+	}
+
+	/** Determines whether the list contains the given object
+		@param o the object to look for
+		@return true if the list contains the object, else false */
+	public boolean contains(Object o) {
+		//TODO
+		return false;
+	}
+
+	/** Retrieves the object from the given index
+		@param index the integer index in the list of the object to retreive
+		@return the object found at the given index */
+	public Object get(int index) {
+		//TODO
+		return null;
+	}
+	/** Determines the first placement of the object in the list
+		@param o the object to look for
+		@return the integer index of the first occurrence of the object in the list */
+	public int indexOf(Object o) {
+		//TODO
+		return -1;
+	}
+
+	/** Determines if the list is empty
+		@return true if the list is empty, else false */
+	public boolean isEmpty() {
+		//TODO
+		return false;
+	}
+
+	/** Determines the last placement of the object in the list
+		@param o the object to look for
+		@return the integer index of the last occurrence of the object in the list */
+	public int lastIndexOf(Object o) {
+		//TODO
+		return -1;
+	}
+
+	/** Removes the given object from the list
+		@param o the object to remove from the list
+		@return true if the removal was successfull, else false */
+	public boolean remove(Object o) {
+		//TODO
+		return false;
+	}
+
+	/** Determines the current size of the list
+		@return the integer number of objects in the list */
+	public int size() {
+		//TODO
+		return -1;
+	}
+
+	/** Removes the object at the given index from the list
+		@param index the integer index of the object to remove
+		@return the object removed from the list */
+	public Object remove(int index) {
+		//TODO
+		return null;
+	}
+
+	/** Sets the given object at the specified index while removing the
+		existing item from its place
+		@param o the object to set in the list
+		@param index the integer index in the list to set the object 
+		@return the object previously contained at the index */
+	public Object set(int index, Object o) {
+		//TODO
+		return null;
+	}
+
+
+}
